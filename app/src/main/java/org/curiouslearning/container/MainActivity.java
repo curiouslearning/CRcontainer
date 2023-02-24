577e6f2eb1822d9e92bcbefc38616cd6d6bf0d43
ashwaraj <35664866+ashishkrr@users.noreply.github.com>
2023-02-24 15:10:20 +0530

    @212

--- app/src/main/java/org/curiouslearning/container/MainActivity.java
+++ app/src/main/java/org/curiouslearning/container/MainActivity.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import com.facebook.FacebookSdk;
 import com.google.firebase.FirebaseApp;
 
+import org.curiouslearning.container.common.SharedPreferencesLiveData;
 import org.curiouslearning.container.data.local.AppManifest;
 import org.curiouslearning.container.data.model.WebApp;
 import org.curiouslearning.container.databinding.ActivityMainBinding;
@@ -25,6 +26,8 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     public ActivityMainBinding binding;
+    public RecyclerView recyclerView;
+    public WebAppsAdapter apps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,17 +45,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         ArrayList<Bitmap> appIcons = new AppManifest().getWebAppsMetaData(getAssets());
+
         HomeViewModal homeViewModal = new HomeViewModal((Application) getApplicationContext(), getAssets());
+
         homeViewModal.getWebApps().observe(this, new Observer<List<WebApp>>() {
             @Override
             public void onChanged(List<WebApp> webApps) {
                 System.out.println(webApps);
             }
         });
-        RecyclerView recyclerView = findViewById(R.id.recycleView);
+        
+        recyclerView = findViewById(R.id.recycleView);
         recyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2, GridLayoutManager.HORIZONTAL, false));
-        WebAppsAdapter apps = new WebAppsAdapter(getApplicationContext(), appIcons);
+        apps = new WebAppsAdapter(getApplicationContext(), appIcons);
         recyclerView.setAdapter(apps);
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        recyclerView.setAdapter(apps);
     }
 }
