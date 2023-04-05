916159d4ab25d704bc07f4c1eebca57ad12ef029
ashwaraj <35664866+ashishkrr@users.noreply.github.com>
2023-04-05 10:42:35 +0530

    #307 rendering remote web apps manifest

--- app/src/main/java/org/curiouslearning/container/MainActivity.java
+++ app/src/main/java/org/curiouslearning/container/MainActivity.java
@@ -1,9 +1,12 @@
 package org.curiouslearning.container;
 
 import android.app.Application;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -11,13 +14,18 @@ import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.google.firebase.FirebaseApp;
 
+import org.curiouslearning.container.data.database.WebAppDatabase;
 import org.curiouslearning.container.data.local.AppManifest;
+import org.curiouslearning.container.data.model.WebApp;
+import org.curiouslearning.container.data.remote.RetrofitInstance;
 import org.curiouslearning.container.databinding.ActivityMainBinding;
 import org.curiouslearning.container.presentation.adapters.WebAppsAdapter;
 import org.curiouslearning.container.presentation.base.BaseActivity;
 import org.curiouslearning.container.presentation.viewmodals.HomeViewModal;
+import org.curiouslearning.container.utilities.ConnectionUtils;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends BaseActivity {
@@ -25,6 +33,8 @@ public class MainActivity extends BaseActivity {
     public ActivityMainBinding binding;
     public RecyclerView recyclerView;
     public WebAppsAdapter apps;
+    public HomeViewModal homeViewModal;
+    private boolean isDataExits;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,25 +47,42 @@ public class MainActivity extends BaseActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        HomeViewModal homeViewModal = new HomeViewModal((Application) getApplicationContext(), getAssets());
 
-        ArrayList<Bitmap> appIcons = new AppManifest().getWebAppsMetaData(getAssets());
+        homeViewModal = new HomeViewModal((Application) getApplicationContext());
+        initRecyclerView();
 
+        homeViewModal.getWebApps().observe(this,  new Observer<List<WebApp>>() {
+            @Override
+            public void onChanged(List<WebApp> webApps) {
+                apps.webApps = webApps;
+                apps.notifyDataSetChanged();
+            }
+        });
+    }
 
+    protected void initRecyclerView() {
         recyclerView = findViewById(R.id.recycleView);
         recyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2, GridLayoutManager.HORIZONTAL, false));
-        apps = new WebAppsAdapter(getApplicationContext(), appIcons, new ArrayList<>());
+        apps = new WebAppsAdapter(getApplicationContext(), new ArrayList<>());
         recyclerView.setAdapter(apps);
-
-        homeViewModal.getWebApps().observe(this, webApps -> {
-            apps.webApps = webApps;
-            apps.notifyDataSetChanged();
-        });
     }
 
     @Override
     public void onResume() {
         super.onResume();
         recyclerView.setAdapter(apps);
+    }
+
+    private void showPrompt(String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message)
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
