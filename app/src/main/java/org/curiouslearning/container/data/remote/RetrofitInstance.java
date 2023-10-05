882c7df7cc6d1983c4188116b6c09efddda05215
amitsinghsutara <102941445+amitsinghsutara@users.noreply.github.com>
2023-10-05 09:26:07 +0530

    #674 Changes

--- app/src/main/java/org/curiouslearning/container/data/remote/RetrofitInstance.java
+++ app/src/main/java/org/curiouslearning/container/data/remote/RetrofitInstance.java
@@ -2,9 +2,12 @@ package org.curiouslearning.container.data.remote;
 
 import org.curiouslearning.container.data.database.WebAppDatabase;
 import org.curiouslearning.container.data.model.WebApp;
+import org.curiouslearning.container.data.model.WebAppV1.WebAppV1;
+import org.curiouslearning.container.data.model.WebAppV2.WebAppV2;
 import org.curiouslearning.container.data.model.WebAppResponse;
 import org.curiouslearning.container.utilities.CacheUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -18,8 +21,10 @@ public class RetrofitInstance {
     private static Retrofit retrofit;
     private static RetrofitInstance retrofitInstance;
 
-    private static String URL = "https://devcuriousreader.wpcomstaging.com/container_app_manifest/prod/";
+    private static String URL = "https://devcuriousreader.wpcomstaging.com/container_app_manifest/testing_1/";
     private List<WebApp> webApps;
+    private List<WebAppV1> webappsv1;
+    private List<WebAppV2> webappsv2;
 
     public static RetrofitInstance getInstance() {
         if (retrofit == null) {
@@ -41,9 +46,8 @@ public class RetrofitInstance {
             public void onResponse(Call<WebAppResponse> call, Response<WebAppResponse> response) {
                 if (response.isSuccessful()) {
                     WebAppResponse webAppResponse = response.body();
-                    CacheUtils.setManifestVersionNumber(webAppResponse.getVersion());
-                    List<WebApp> webApps = webAppResponse.getWebApps();
-                    webAppDatabase.deleteWebApps(webApps);
+                    processWebAppResponse(webAppResponse, webAppDatabase);
+
                 }
             }
 
@@ -52,6 +56,52 @@ public class RetrofitInstance {
                 System.out.println(t.getMessage() + "Something went wrong");
             }
         });
+    }
+
+    private void processWebAppResponse(WebAppResponse webAppResponse, WebAppDatabase webAppDatabase) {
+        String manifestVersion = webAppResponse.getVersion();
+        Integer majorVersion = Integer.parseInt(manifestVersion.split("\\.")[0]);
+        CacheUtils.setManifestVersionNumber(manifestVersion);
+
+        List<WebApp> webApps = new ArrayList<>();
+
+        if (majorVersion == 1) {
+            for (WebAppV1 webAppV1 : webAppResponse.getWebAppsV1()) {
+                WebApp convertedWebApp = convertV1ToWebApp(webAppV1);
+                webApps.add(convertedWebApp);
+            }
+        } else if (majorVersion == 2) {
+            for (WebAppV2 webAppV2 : webAppResponse.getWebAppsV2()) {
+                WebApp convertedWebApp = convertV2ToWebApp(webAppV2);
+                webApps.add(convertedWebApp);
+            }
+        } else {
+            webApps = webAppResponse.getWebApps();
+        }
+
+        webAppDatabase.deleteWebApps(webApps);
+    }
+
+    public WebApp convertV1ToWebApp(WebAppV1 webAppV1) {
+        WebApp webApp = new WebApp();
+        webApp.setAppId(webAppV1.getAppId());
+        webApp.setTitle(webAppV1.getTitle());
+        webApp.setLanguage(webAppV1.getLanguage());
+        webApp.setAppUrl(webAppV1.getAppUrl());
+        webApp.setAppIconUrl(webAppV1.getAppIconUrl());
+
+        return webApp;
+    }
+
+    public WebApp convertV2ToWebApp(WebAppV2 webAppV2) {
+        WebApp webApp = new WebApp();
+        webApp.setAppId(webAppV2.getAppId());
+        webApp.setTitle(webAppV2.getTitle());
+        webApp.setLanguage(webAppV2.getLanguage());
+        webApp.setAppUrl(webAppV2.getAppUrl());
+        webApp.setAppIconUrl(webAppV2.getAppIconUrl());
+
+        return webApp;
     }
 
     public void fetchAndCacheWebApps(WebAppDatabase webAppDatabase) {
