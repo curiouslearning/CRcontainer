9501e1f52199b72027d5a0f208c7166f185bac52
Nikhil77sutara <nikhilchoudhary@sutara.org>
2024-07-17 16:47:22 +0530

    updated code

--- app/src/main/java/org/curiouslearning/container/installreferrer/InstallReferrerManager.java
+++ app/src/main/java/org/curiouslearning/container/installreferrer/InstallReferrerManager.java
@@ -81,7 +81,7 @@ public class InstallReferrerManager {
     }
     private Map<String, String> extractReferrerParameters(String referrerUrl) {
         Map<String, String> params = new HashMap<>();
-        
+        //create a dummmy url
         // Using a dummy URL to ensure `Uri.parse` correctly processes the referrerUrl as part of a valid URL.
         Uri uri = Uri.parse("http://dummyurl.com/?" +referrerUrl);
         String deeplink= uri.getQueryParameter("deferred_deeplink");
