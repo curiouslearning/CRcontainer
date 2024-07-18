fb4c2be4c4bc96d849baef24f5878a642ed479b1
Nikhil77sutara <nikhilchoudhary@sutara.org>
2024-07-18 13:45:19 +0530

    Revert "updated"
    This reverts commit ddb22278f70dd9cb11ec1dc61b76d311ab248499.

--- app/src/main/java/org/curiouslearning/container/firebase/AnalyticsUtils.java
+++ app/src/main/java/org/curiouslearning/container/firebase/AnalyticsUtils.java
@@ -77,7 +77,7 @@ public class AnalyticsUtils {
 
     private  static Map<String, String> extractReferrerParameters(String referrerUrl) {
         Map<String, String> params = new HashMap<>();
-        Uri uri = Uri.parse(referrerUrl);
+        Uri uri = Uri.parse("http://dummyurl.com/?" +referrerUrl);
 
         String source = uri.getQueryParameter("utm_source");
         String campaign = uri.getQueryParameter("utm_campaign");
@@ -85,7 +85,7 @@ public class AnalyticsUtils {
         Log.d("data without decode", campaign + " " + source + " " + content);
         content = urlDecode(content);
 
-        Log.d("referral data", campaign + " " + source + " " + content);
+        Log.d("referral data", uri+" "+campaign + " " + source + " " + content+" "+referrerUrl);
 
         params.put("source", source);
         params.put("campaign", campaign);
