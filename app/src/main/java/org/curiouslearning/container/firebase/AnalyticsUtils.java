c2a7604d03334e48b73f74413933f54530d24ac8
Nikhil77sutara <nikhilchoudhary@sutara.org>
2024-07-22 12:51:51 +0530

    updated AnalyticsUtil

--- app/src/main/java/org/curiouslearning/container/firebase/AnalyticsUtils.java
+++ app/src/main/java/org/curiouslearning/container/firebase/AnalyticsUtils.java
@@ -1,6 +1,7 @@
 package org.curiouslearning.container.firebase;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,6 +19,9 @@ import java.util.Map;
 public class AnalyticsUtils {
 
     private static FirebaseAnalytics mFirebaseAnalytics;
+    private static final String PREFS_NAME = "InstallReferrerPrefs";
+    private static final String UTM_SOURCE = "utm_source";
+    private static final String UTM_CAMPAIGN = "utm_campaign";
 
     public static FirebaseAnalytics getFirebaseAnalytics(Context context) {
         if (mFirebaseAnalytics == null) {
@@ -29,7 +33,7 @@ public class AnalyticsUtils {
     public static void logEvent(Context context, String eventName, String appName, String appUrl, String pseudoId, String language) {
         FirebaseAnalytics firebaseAnalytics = getFirebaseAnalytics(context);
 
-        Bundle bundle = new Bundle();
+        Bundle bundle = createEventBundle(context);
         bundle.putString("web_app_title", appName);
         bundle.putString("web_app_url", appUrl);
         bundle.putString("cr_user_id", pseudoId);
@@ -40,7 +44,7 @@ public class AnalyticsUtils {
     public static void logLanguageSelectEvent(Context context, String eventName, String pseudoId, String language, String manifestVersion, String autoSelected) {
         FirebaseAnalytics firebaseAnalytics = getFirebaseAnalytics(context);
 
-        Bundle bundle = new Bundle();
+        Bundle bundle = createEventBundle(context);
         long currentEpochTime = getCurrentEpochTime();
         bundle.putLong("event_timestamp", currentEpochTime);
         bundle.putString("cr_user_id", pseudoId);
@@ -55,24 +59,40 @@ public class AnalyticsUtils {
     public static void logReferrerEvent(Context context, String eventName, ReferrerDetails response) {
         if (response != null) {
             FirebaseAnalytics firebaseAnalytics = getFirebaseAnalytics(context);
-           
+            String referrerUrl = referrerDetails.getInstallReferrer();
             Bundle bundle = new Bundle();
-            bundle.putString("referrer_url", response.getInstallReferrer());
+            bundle.putString("referrer_url", referrerUrl);
             bundle.putLong("referrer_click_time", response.getReferrerClickTimestampSeconds());
             bundle.putLong("app_install_time", response.getInstallBeginTimestampSeconds());
-
-            Map<String, String> extractedParams = extractReferrerParameters(response.getInstallReferrer());
+            
+            Map<String, String> extractedParams = extractReferrerParameters(referrerUrl);
             if (extractedParams != null) {
                 String source = extractedParams.get("source");
                 String campaign = extractedParams.get("campaign");
                 String content = extractedParams.get("content");
-                bundle.putString("source", source);
-                bundle.putString("campaign", campaign);
-                bundle.putString("content", content);
+                bundle.putString("utm_source", source);
+                bundle.putString("utm_campaign", campaign);
+                bundle.putString("utm_content", content);
+                storeReferrerParams(context, source, campaign);
             }
 
             firebaseAnalytics.logEvent(eventName, bundle);
         }
+    }
+    private static void storeReferrerParams(Context context, String source, String campaign) {
+        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(UTM_SOURCE, source);
+        editor.putString(UTM_CAMPAIGN, campaign);
+        editor.apply();
+    }
+
+    private static Bundle createEventBundle(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        Bundle bundle = new Bundle();
+        bundle.putString("utm_source", prefs.getString(UTM_SOURCE, ""));
+        bundle.putString("utm_campaign", prefs.getString(UTM_CAMPAIGN, ""));
+        return bundle;
     }
 
     private  static Map<String, String> extractReferrerParameters(String referrerUrl) {
@@ -83,7 +103,7 @@ public class AnalyticsUtils {
         String source = uri.getQueryParameter("utm_source");
         String campaign = uri.getQueryParameter("utm_campaign");
         String content = uri.getQueryParameter("utm_content");
-        Log.d("data without decode", campaign + " " + source + " " + content);
+        Log.d("data without decode util", campaign + " " + source + " " + content);
         content = urlDecode(content);
 
         Log.d("referral data", uri+" "+campaign + " " + source + " " + content+" "+referrerUrl);
