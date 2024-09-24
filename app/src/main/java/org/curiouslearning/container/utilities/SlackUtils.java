042f137aaae83c4a5a36bae8debacb2f9e6900ba
Rajesh <kumar@sutara.org>
2024-09-24 16:00:11 +0530

    Added env to gitignore

--- app/src/main/java/org/curiouslearning/container/utilities/SlackUtils.java
+++ app/src/main/java/org/curiouslearning/container/utilities/SlackUtils.java
@@ -1,5 +1,7 @@
 package org.curiouslearning.container.utilities;
 import android.os.AsyncTask;
+
+import io.github.cdimascio.dotenv.Dotenv;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -7,7 +9,8 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class SlackUtils {
-    private static final String SLACK_WEBHOOK_URL = "";  // Replace with your Webhook URL
+//    private static final String SLACK_WEBHOOK_URL = "";  // Replace with your Webhook URL
+
     private static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
     // Method to send message to Slack
@@ -19,12 +22,20 @@ public class SlackUtils {
     private static class SendSlackMessageTask extends AsyncTask<String, Void, Void> {
         @Override
         protected Void doInBackground(String... messages) {
+            Dotenv dotenv = Dotenv.configure()
+                    .directory("/assets")
+                    .filename("env") // instead of '.env', use 'env'
+                    .load();
+            String webHookUrl = dotenv.get("SLACK_WEBHOOK_URL");
+            System.out.println("slackURLTesting : "+webHookUrl);
+
+
             OkHttpClient client = new OkHttpClient();
             String jsonPayload = "{\"text\": \"" + messages[0] + "\"}";
 
             RequestBody body = RequestBody.create(JSON, jsonPayload);
             Request request = new Request.Builder()
-                    .url(SLACK_WEBHOOK_URL)
+                    .url(webHookUrl)
                     .post(body)
                     .build();
 
