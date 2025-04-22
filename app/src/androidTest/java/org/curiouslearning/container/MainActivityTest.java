690eebf4ec305d583587f02065c76b30f861ca28
Nikhil77sutara <nikhilchoudhary@sutara.org>
2025-04-22 11:18:51 +0530

    Update MainActivityTest.java

--- app/src/androidTest/java/org/curiouslearning/container/MainActivityTest.java
+++ app/src/androidTest/java/org/curiouslearning/container/MainActivityTest.java
@@ -11,9 +11,15 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.startsWith;
+import static org.hamcrest.object.HasToString.hasToString;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
@@ -24,8 +30,38 @@ public class MainActivityTest {
 
     @Test
     public void test_languagePopupIsShown_andLanguagesDisplayed() {
-        SystemClock.sleep(2000);
         onView(withId(R.id.autoComplete))
+                .check(matches(isDisplayed()));
+
+    }
+
+    @Test
+    public void test_clickDropdown_showsMenu() {
+        onView(withId(R.id.dropdown_menu)).perform(click());
+
+    }
+
+    //    @Test
+//    public void test_dropdownIsScrollable() {
+//        SystemClock.sleep(2000);
+//        onView(withId(R.id.dropdown_menu)).perform(click());
+//        SystemClock.sleep(2000);
+//        onData(anything())
+//                .inRoot(RootMatchers.isPlatformPopup())
+//                .atPosition(55)
+//                .check(matches(isDisplayed()));
+//    }
+    @Test
+    public void test_dropdownIsScrollableToZulu() {
+        SystemClock.sleep(2000);
+
+        // Open the dropdown
+        onView(withId(R.id.dropdown_menu)).perform(click());
+        SystemClock.sleep(1000);
+
+        // Scroll and verify that the item with "Zulu" is displayed
+        onData(hasToString(startsWith("Isizulu")))
+                .inRoot(RootMatchers.isPlatformPopup())
                 .check(matches(isDisplayed()));
     }
 }
