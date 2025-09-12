0306005ae3ff017849b1566014f74204bf3e5557
amitsinghsutara <amit@sutara.org>
2025-09-12 14:24:18 +0530

    Inititializing Sentry for error logging!

--- app/src/main/java/org/curiouslearning/container/MyApplication.java
+++ app/src/main/java/org/curiouslearning/container/MyApplication.java
@@ -8,10 +8,19 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-//        FacebookSdk.sdkInitialize(getApplicationContext());
+        // FacebookSdk.sdkInitialize(getApplicationContext());
         FacebookSdk.setAutoInitEnabled(true);
         FacebookSdk.fullyInitialize();
         FacebookSdk.setAdvertiserIDCollectionEnabled(true);
         AppEventsLogger.activateApp(this);
+        SentryAndroid.init(this, options -> {
+            options.setDsn(
+                    "https://3e3bfa9bd4473edd4e0b0d502195f4de@o4504951275651072.ingest.us.sentry.io/4510001311383552"); // replace
+                                                                                                                        // with
+                                                                                                                        // your
+                                                                                                                        // DSN
+            options.setEnvironment(BuildConfig.BUILD_TYPE);
+
+        });
     }
 }
