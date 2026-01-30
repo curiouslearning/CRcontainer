e032b1b2e170d7699095e9c4e75f3a6fc5ca1f55
George <dzavashviligeorge@gmail.com>
2026-01-30 20:10:39 +0400

    fix: review comment 2, rive animation initialization

--- app/src/main/java/org/curiouslearning/container/MyApplication.java
+++ app/src/main/java/org/curiouslearning/container/MyApplication.java
@@ -3,8 +3,6 @@ package org.curiouslearning.container;
 import android.app.Application;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
-import androidx.startup.AppInitializer;
-import app.rive.runtime.kotlin.RiveInitializer;
 
 import io.sentry.android.core.SentryAndroid;
 
@@ -27,8 +25,7 @@ public class MyApplication extends Application {
             options.setEnvironment(BuildConfig.BUILD_TYPE);
 
         });
-        AppInitializer.getInstance(this)
-                .initializeComponent(RiveInitializer.class);
+        // RiveInitializer is auto-initialized via AndroidManifest.xml (InitializationProvider)
 
     }
 }
